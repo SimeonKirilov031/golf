19b95219368bc5ce07b93b3fa314d89fa884e49c
Simeon Kirilov <simeon.kirilov@forkpoint.com>
2019-09-27 09:02:07 +0300

    Polish

--- Simeon/js/main.js
+++ Simeon/js/main.js
@@ -3,6 +3,15 @@ var images = ['images/slide1.jpg', 'images/slide2.jpg', 'images/slide3.jpg'];
 
 function slide(index){
     for (let i = 0; i < images.length; i++) {
-        document.getElementById("slide" + (i + 1)).src= images[(index + i) % images.length];
+        document.getElementById(`slide${i + 1}`).src= images[(index + i) % images.length];
     }
 };
+
+function categorySwap(catNumber){
+    var categories = [document.getElementById('plp-filter-category-1'), document.getElementById('plp-filter-category-2'),
+    document.getElementById('plp-filter-category-3'), document.getElementById('plp-filter-category-4'), document.getElementById('plp-filter-category-5')];
+
+    for(var i=0; i < categories.length; i++){
+        categories[i].classList.remove('plp-filter-marked');
+    }
+}
